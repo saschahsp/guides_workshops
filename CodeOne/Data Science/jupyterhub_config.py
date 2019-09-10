adf4c0a6750626448c5aeb52a29b423932a08cf7
Sascha Hagedorn <saschah@gmx.net>
2019-09-10 14:31:43 +0200

    Add files via upload

---
+++ CodeOne/Data Science/jupyterhub_config.py
@@ -0,0 +1,21 @@
+﻿# jupyterhub_config.py
+c = get_config()
+
+# HINT: you can switch here between JupyterLab and JupyterNotebook
+# JupyterLab
+c.Spawner.default_url = '/lab'
+# JupyterNotebook
+#c.Spawner.default_url = '/tree'
+c.Spawner.debug = True
+
+# ip is deprecated
+c.JupyterHub.db_url = '/data/jupyterhub/jupyterhub.sqlite'
+c.JupyterHub.ip = '0.0.0.0'
+c.JupyterHub.port = 8000
+c.JupyterHub.admin_access = True
+
+# Specify users and admin
+# Allow everyone to login
+#c.Authenticator.whitelist = {"admin"}
+c.Authenticator.admin_users = {"admin"}
+
